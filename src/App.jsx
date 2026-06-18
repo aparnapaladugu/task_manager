a75af0b2c017b64c8b0663a18ed1dbf715b51d48
aparna paladugu <aparnachowdary72837@gmail.com>
2026-06-19 00:14:25 +0530

    Done with the form

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,27 @@
+import { useEffect, useState } from "react";
+import TaskForm from "./components/TaskForm";
+
 function App() {
+  const [tasks, setTasks] = useState(() => {
+    const oldTasks = localStorage.getItem("tasks");
+    return oldTasks ? JSON.parse(oldTasks) : [];
+  });
+  const addTasks = (task) => {
+    setTasks((prev) => [...prev, task]);
+  };
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+  // const editTask = (editItem,id) => {
+  //   setTasks((item)=> item.id == id ? {editItem} : item)
+  // };
+  // const deleteTask = (id) =>{
+  //   setTasks(item => item.id != id)
+  // }
   return (
-    <div>
+    <div class="h-full w-full bg-green-300 flex items-center jsutify-center">
       <h1>expense tracker</h1>
+      <TaskForm addTasks={addTasks} />
     </div>
   );
 }
--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -1,7 +1,51 @@
-function TaskForm() {
+import { useState } from "react";
+
+function TaskForm({ addTasks }) {
+  const [taskForm, setTaskForm] = useState({
+    id: "",
+    title: "",
+    description: "",
+    priority: "",
+    status: "",
+    duedate: "",
+  });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!taskForm.duedate || !taskForm.priority || !taskForm.title) {
+      return;
+    }
+    addTasks(taskForm);
+    setTaskForm({
+      id: "",
+      title: "",
+      description: "",
+      priority: "",
+      status: "",
+      duedate: "",
+    });
+  };
+  const handleChange = (e) => {
+    console.log(e, "on change field");
+    setTaskForm({
+      ...taskForm,
+      [e.target.name]: e.target.value,
+    });
+  };
   return (
-    <div>
+    <div className="h-full w-full flex items-center justify-center">
       <h2>Task Form</h2>
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} value={taskForm.title} name="title"></input>
+        <input onChange={handleChange} value={taskForm.description} name="description"></input>
+        <select className="border p-3 rounded-lg" value={taskForm.priority} onChange={handleChange}>
+          <option value="">Select Priority</option>
+          <option value="high">High</option>
+          <option value="medium">Medium</option>
+          <option value="low">Low</option>
+        </select>
+        <input type="date" name="date" value={taskForm.duedate} onChange={handleChange} className="border p-3 rounded-lg"></input>
+        <button className="mt-4 bg-blue-500 text-white px-5 py-2 rounded-lg">Add Task</button>
+      </form>
     </div>
   );
 }
